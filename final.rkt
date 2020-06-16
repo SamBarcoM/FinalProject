1af4cc88c293726d38a6aaca30e471fd95e708f4
emihdz97 <21973251+emihdz97@users.noreply.github.com>
2020-06-15 22:15:30 -0500

    Add files via upload

--- final.rkt
+++ final.rkt
@@ -21,8 +21,7 @@
 #\  #\! #\_ #\@ #\$ #\" #\' #\A #\B #\C
 #\D #\E #\F #\G #\H #\I #\J #\K #\L #\M
 #\N #\O #\P #\Q #\R #\S #\T #\U #\V #\W
-#\X #\Y #\Z #\/ #\= #\; #\( #\: #\) #\# 
-#\\ #\+ #\* #\| #\< #\> #\[ #\] #\% ))
+#\X #\Y #\Z ))
 
 
 (define (decipher-document input-path output-path n key)
